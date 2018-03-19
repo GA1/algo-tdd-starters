531568b5527325141da9452f640da66cef521099
GA1 <nvkga1@gmail.com>
2018-03-19 10:49:57 +0100

    Add algo-starter for java

---
+++ java/src/main/java/Task1.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Task1 {
+
+    public int solve() {
+        return 13;
+    }
+}
---
+++ java/src/test/java/Task1Test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Task1Test {
+
+    private Task1 task1 = new Task1();
+
+    @Test
+    public void test1element() {
+        assertEquals(13, task1.solve());
+    }
+
+}
