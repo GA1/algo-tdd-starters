aa0928228f4e11fc4936ddde94fd9a8a5e9f1705
GA1 <nvkga1@gmail.com>
2018-03-19 11:03:42 +0100

    Add algo-starter for python

---
+++ python/src/Task1.py
@@ -0,0 +1,4 @@
+
+
+def solve():
+    return 13
---
+++ python/test/test_utils.py
@@ -0,0 +1,9 @@
+import unittest
+from src.Task1 import solve
+
+
+class TestTask1(unittest.TestCase):
+
+    def test_no_ids(self):
+        self.assertEqual(13, solve())
+
