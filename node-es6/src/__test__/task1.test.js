ac7c2a2730d05b65f37319a9f0b2b7f468ae4109
GA1 <nvkga1@gmail.com>
2018-05-24 18:02:13 +0200

    Use 'describe' and 'it' jest idioms

--- node-es6/src/__test__/task1.test.js
+++ node-es6/src/__test__/task1.test.js
@@ -1,5 +1,11 @@
 var task1 = require('../task1');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(task1.solve(2)).toEqual([4, 10, 14]);
-});
+describe('task 1 solver', () => {
+  it('should return 13', () => {
+  	expect(task1.solveWithEs6Feature(2)).toEqual([4, 10, 14]);
+  });
+
+  it('should be able to map an array', () => {
+    expect(task1.solveWithEs6Feature(2)).toEqual([4, 10, 14]);
+  });
+})
--- node-es6/src/task1.js
+++ node-es6/src/task1.js
@@ -1,6 +1,11 @@
 
 function solve(a) {
+  return 13
+}
+
+function solveWithEs6Feature(a) {
   return [a, 5, 7].map(n => n*2)
 }
 
 module.exports.solve = solve;
+module.exports.solveWithEs6Feature = solveWithEs6Feature;
