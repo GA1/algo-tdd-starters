4c33ff97a899da0fbf4027d8b7ea6a45e5ab8cce
GA1 <nvkga1@gmail.com>
2020-01-30 10:44:32 +0100

    Add prettier

--- node-es6/src/__test__/task2.test.js
+++ node-es6/src/__test__/task2.test.js
@@ -1,9 +1,7 @@
 const task2 = require('../task2')
 
 describe('task 2 solver', () => {
-
   it('should return 11', () => {
-  	expect(task2.solve()).toEqual(11)
+    expect(task2.solve()).toEqual(11)
   })
-
 })
