19453a1fc3b00c1fc363cde206873b7b6acd2647
Kamil Nguyen Van <nvkga1@gmail.com>
2023-06-21 11:06:19 +0200

    make typescript starter work after upgrading dependencies

--- typescript/jest.config.js
+++ typescript/jest.config.js
@@ -1,12 +1,5 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  "roots": [
-    "<rootDir>/"
-  ],
-  "testMatch": [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  },
-}
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
